399c9db967c9573edf0cede2eb306c385bb77708
MD Shafi Hamid <sh611592@gmail.com>
2026-04-10 19:51:13 +0530

    updated API URL

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,7 @@ import pandas as pd
 import time
 
 # ------------------ CONFIG ------------------
-API_URL = "http://127.0.0.1:8000"
+API_URL = "http://13.49.78.44:8000"
 
 st.set_page_config(page_title="Real-Time Dashboard", layout="wide")
 
